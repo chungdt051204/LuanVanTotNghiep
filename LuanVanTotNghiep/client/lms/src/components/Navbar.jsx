bc00e6cdee6b6a9cb49e206c1cf8ecc6b0f41f0b
chungdt051204 <email_github_ca_bn@gmail.com>
2026-08-26 11:21:39 +0700

    Bổ sung giao diện responsive cho trang Landing Page, Footer, List Course, danh sách khóa học và trang khóa học của tôi

--- LuanVanTotNghiep/client/lms/src/components/Navbar.jsx
+++ LuanVanTotNghiep/client/lms/src/components/Navbar.jsx
@@ -157,7 +157,7 @@ export const Navbar = () => {
           </div>
           {/* SEARCH BAR */}
           {currentRole === "user" && (
-            <div className="relative  lg:w-[32%] md:w-[60%] sm:w-[45%]">
+            <div className="relative w-[43%] lg:w-[32%] md:w-[60%]">
               <div className="flex gap-x-2 items-center py-1.5 sm:py-2 px-3 sm:px-4 bg-surface-bg rounded-[8px]">
                 <IoSearch
                   onClick={() => {
@@ -287,7 +287,7 @@ export const Navbar = () => {
             ))}
           </ul>
           {/* ICONS & ACTIONS */}
-          <div className="flex items-center gap-x-4 sm:gap-x-6">
+          <div className="flex items-center gap-x-3 lg:gap-x-6 md:gap-x-6">
             {isLogin && currentRole === "user" && (
               <div
                 className="relative cursor-pointer"
